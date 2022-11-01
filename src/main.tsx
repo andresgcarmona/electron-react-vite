8d7335eeb2884f8f509b8cd9f903bfa65925d34f
Andrés G. Carmona <ing.andres.carmona@gmail.com>
2022-11-01 15:56:22 -0600

    Added sample code.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import './samples/node-api'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
